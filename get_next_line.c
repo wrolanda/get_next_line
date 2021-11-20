aae83eb7c33e5506c082a69b7387496a25ca224b
Whatever Rolanda <wrolanda@oa-c1.msk.21-school.ru>
2021-11-20 15:31:08 +0300

    something (day 0)

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,22 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 13:30:16 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/20 13:36:04 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/20 15:23:46 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char	*get_next_line(int fd)
 {
+	char	*buf;
+	size_t	BUFFER_SIZE;
+	ssize_t	check_read_byte;
 
+	check_read_byte = 0;
+	BUFFER_SIZE = 1;
+	check_read_byte = read(fd, buf, BUFFER_SIZE);
+	if (check_read_byte == 0)
+		return (NULL);
 }
+/*ssize_t read(int fd, void *buf, size_t count);*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,16 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 13:31:16 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/11/20 13:35:52 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/11/20 15:15:28 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
+/*for read*/
+# include <unistd.h>
+/*for ssize_t*/
+# include <sys/types.h>
 char	*get_next_line(int fd);
 
 #endif

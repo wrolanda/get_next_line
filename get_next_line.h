4a957f64833e04c3dbaffd0bbbb6fa1cde54826d
wrolanda <Nikita.Mitriackov@gmail.com>
2021-12-14 18:03:53 +0300

    beta version(tripouille ok)

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 13:31:16 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/12/14 15:30:34 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/12/14 18:02:31 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@
 /*for max min value of type variables*/
 # include <limits.h>
 char	*get_next_line(int fd);
-char	*ft_strchr(const char *s, int c);
-int		ft_strlen(char *str);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *str);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(char *s1, char *s2);
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: wrolanda <wrolanda@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 13:31:01 by wrolanda          #+#    #+#             */
-/*   Updated: 2021/12/14 15:25:07 by wrolanda         ###   ########.fr       */
+/*   Updated: 2021/12/14 18:02:37 by wrolanda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ DESCRIPTION
 	considered to be part of the string; therefore if c is `\0', the func-
 	tions locate the terminating `\0'.*/
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(char *s, int c)
 {
 	if (!s)
 		return (0);
@@ -34,9 +34,9 @@ char	*ft_strchr(const char *s, int c)
 	return ((char *)0);
 }
 
-int	ft_strlen(char *str)
+size_t	ft_strlen(char *str)
 {
-	int	i;
+	size_t	i;
 
 	i = 0;
 	if (!str)
@@ -72,40 +72,6 @@ void	*ft_calloc(size_t count, size_t size)
 	return (p);
 }
 
-/*strlcpy, strlcat -- size-bounded string copying and concatenation
-DESCRIPTION
-	strlcat() appends string src to the end of dst.  It will append at most
-	dstsize - strlen(dst) - 1 characters.  It will then NUL-terminate, unless
-	dstsize is 0 or the original dst string was longer than dstsize (in prac-
-	tice this should not happen as it means that either dstsize is incorrect
-	or that dst is not a proper string).*/
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
-{
-	size_t	i;
-	size_t	j;
-	size_t	res_dst;
-	size_t	res_src;
-
-	i = 0;
-	res_dst = ft_strlen(dst);
-	res_src = ft_strlen((char *)src);
-	j = res_dst;
-	if (dstsize <= 0)
-		return (res_src + dstsize);
-	while (src[i] && j < dstsize - 1)
-	{
-		dst[j] = src[i];
-		i++;
-		j++;
-	}
-	if (dstsize != 0 && res_dst <= dstsize)
-		dst[j] = '\0';
-	if (dstsize < res_dst)
-		return (res_src + dstsize);
-	else
-		return (res_dst + res_src);
-}
-
 /*
 DESCRIPTION
 Allocates (with malloc(3)) and returns a new
@@ -118,45 +84,29 @@ PARAMETERS
 */
 char	*ft_strjoin(char *s1, char *s2)
 {
-	char	*newstr;
 	size_t	i;
+	size_t	j;
+	char	*newstr;
 
-	if (s1 == NULL)
+	if (!s1)
 	{
 		s1 = (char *)malloc(sizeof(char) * 1);
 		s1[0] = '\0';
 	}
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-	i = ft_strlen((char *)s1) + ft_strlen((char *)s2);
+	i = ft_strlen(s1) + ft_strlen(s2);
 	newstr = ft_calloc(sizeof(char), i + 1);
 	if (newstr == NULL)
 		return (NULL);
-	ft_strlcat(newstr, s1, i + 1);
-	ft_strlcat(newstr, s2, i + 1);
+	i = -1;
+	j = 0;
+	if (s1)
+		while (s1[++i] != '\0')
+			newstr[i] = s1[i];
+	while (s2[j] != '\0')
+		newstr[i++] = s2[j++];
+	newstr[ft_strlen(s1) + ft_strlen(s2)] = '\0';
+	free(s1);
 	return (newstr);
 }
-
-/*memset - fill a byte string with a byte value
-DESCRIPTION
-	The memset() function writes len bytes of value c (converted to an
-	unsigned char) to the string b.*/
-/*void	*ft_memset(void *b, int c, size_t len)
-{
-	size_t			i;
-	unsigned char	*str;
-
-	str = b;
-	i = 0;
-	while (i < len)
-	{
-		str[i] = c;
-		i++;
-	}
-	return (b);
-}
-
-void	ft_bzero(void *s, size_t n)
-{
-	ft_memset(s, 0, n);
-}*/
